d90c9ff71ca0f7bf4f4ec620ff966116f971c9ad
DaianCosta <desenvolvimentodaian@gmail.com>
2022-07-18 11:37:18 -0300

    adjust

--- src/main/java/com/daiancosta/brokeragenote/domain/entities/FileInfo.java
+++ src/main/java/com/daiancosta/brokeragenote/domain/entities/FileInfo.java
@@ -3,8 +3,6 @@ package com.daiancosta.brokeragenote.domain.entities;
 import com.daiancosta.brokeragenote.domain.entities.enums.TypeFileEnum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.io.Serializable;
 
